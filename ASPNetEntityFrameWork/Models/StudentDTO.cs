55db185a96de2588ea6bb1a5ad8197620e1c631c
intikhab-h-bhat <intikhabh@gmail.com>
2024-08-13 09:28:03 +0530

    Built in attribute for validationb

--- ASPNetEntityFrameWork/Models/StudentDTO.cs
+++ ASPNetEntityFrameWork/Models/StudentDTO.cs
@@ -1,16 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Cache;
 
 namespace ASPNetEntityFrameWork.Models
 {
     public class StudentDTO
     {
         public int Id { get; set; }
-        [Required]
+        [Required (ErrorMessage ="Please enter the student Name")]
+        [StringLength (20)]
         public string Name { get; set; }    
         public string Address { get; set; }
-        [EmailAddress]
+        [EmailAddress(ErrorMessage ="Please enter valid email addres")]
         public string Email { get; set; }
         public DateTime DOB { get; set; }
+        [Range(10,20)]
+        public int Age {  get; set; }
+
+        public string Password {  get; set; }
+
+        [Compare(nameof(Password))]
+        public string ConfirmPassword {  get; set; }
 
     }
 }
