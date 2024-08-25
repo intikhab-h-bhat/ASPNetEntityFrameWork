db763fc026fa241ee6bea9b517c34d310c1fcefe
intikhab-h-bhat <intikhabh@gmail.com>
2024-08-25 12:08:36 +0530

    Configure the JWT in program.cs file

--- ASPNetEntityFrameWork/Program.cs
+++ ASPNetEntityFrameWork/Program.cs
@@ -1,5 +1,8 @@
 using ASPNetEntityFrameWork.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders();
@@ -17,6 +20,26 @@ builder.Services.AddControllers(options => options.ReturnHttpNotAcceptable =true
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//JWT authentication
+var key = Encoding.ASCII.GetBytes(builder.Configuration.GetValue<string>("JWTSceret"));
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken  = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+    };
+
+});
 
 var app = builder.Build();
 
